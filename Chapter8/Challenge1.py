3935ce9d13d28f6fe09b78f914b5d6152c2334d0
松本英人 <hide@matsumotohideto-no-MacBook-Pro.local>
2020-08-25 23:33:39 +0900

    add Chapter8

---
+++ Chapter8/Challenge1.py
@@ -0,0 +1,3 @@
+import statistics
+number = [1,2,3,4,5,5,5,3,3,]
+print(statistics.median_grouped(number))
---
+++ Chapter8/cubed.py
@@ -0,0 +1,5 @@
+def abcde(x):
+    return x ** 3
+
+if __name__ == "__main__":
+    print("{0}".format(abcde(4)))
